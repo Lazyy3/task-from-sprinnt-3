f58bb0eb7533152afb900ee76a6f4f7c86ac37b3
Lazyy3 <kzuhov@mail.ru>
2022-11-04 23:06:47 +0300

    1.Изменено название лейбла по порядку название+тип переменной 2.Добавлены модификаторы приватности переменным и функциям

--- task3/ViewController.swift
+++ task3/ViewController.swift
@@ -9,24 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var counterValue: UILabel!
-    override func viewDidLoad() {
+     
+    @IBOutlet weak var counterLabel: UILabel!
+    internal override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        counterValue.text = "Значение счётчика: \(value)"
+        counterLabel.text = "Значение счётчика: \(value)"
     }
    
-    var value: Int = 0
+    private var value: Int = 0
    
-    @IBAction func tapToChangeValue(_ sender: UIButton) {
-        for i in 0..<1 {
-            if value >= i {
-                value += 1
-            }
-        }
-        return counterValue.text = "Значение счётчика: \(value)"
+    @IBAction private func tapToChangeValue(_ sender: UIButton) {
+       value += 1
+        return counterLabel.text = "Значение счётчика: \(value)"
     }
-    
     
 }
 
